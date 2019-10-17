8da9f419220f3c5b02f9d8586530d30ecb7097c2
ahmadsoliman <ahmad.soliman@thoughtdesign.com.au>
2019-10-17 23:03:04 +0200

    add db.settings for project testing purposes

---
+++ backend/db.settings.js
@@ -0,0 +1,18 @@
+module.exports = { 
+  user: 'admin',
+  password: 'P@ss1234',
+  dbUrl: 'mongodb+srv://admin:Pass1234@toptal-interview-9h2hw.mongodb.net/CaloriesTracker?retryWrites=true&w=majority',
+  jwtSecret: `MIICXAIBAAKBgQCnoKy0yg+XCAeVWJ0KTT9P9o2WYPFDryX3srfKoCCro0zTMh80
+              EV1wfrTl1uTb2hwIKRqsDch2wTwwnqgQI9gutWCAyXDo/h90aH/uDCS4V9LS213Y
+              dnwlwoN4vu7ER/3KpK8VpVzLueRuFFbP6ELFC6ahppmjbywc1NApB8VJSQIDAQAB
+              AoGAKDKbt+8Mtgcy6lmJr0K/AAn8AifVM4n47OFquaiUrRRIBA1s9iuD4MpBzywt
+              TZuMRVi2ELRtciz8phwl6VjvKkCQPwJ32+96a46n3+Z/wx2C+u5JdiKETGfMKDSk
+              wW2Gxd226z11KZjFAeEn+o5NAWklgBD4PUetCZS+yhZ8/dECQQDiIQZ5t8e97qXF
+              p/SHpbWxkXvnSCDeTbKNxlhQWzm1CWOoYnT68ogHx24YgshEe2Xtb0eMD86IAMs3
+              IAAKEOm/AkEAvcVQ9T06aXYmQQ2OjpnUG19enwyWoCfzsXBv+ZrOGqwnUdZJrPMb
+              ydpd53gZQpKIyHzrXMwwg7PdzcSCCgi+9wJBAN3VUCjwJb8p8u4oijbYVZpRAqAt
+              njD3QAbdYZ3TIJV9FIsHq4LTkqcu+c6J3jVwaNm4pRO+HfpF7RWzCtbVKLkCQFvM
+              FQtsYNMfGgYh3rHQa7AKBCV/22UBrnpI34GohsaUPDj0JZOJzglonkAX8nAnng8N
+              jnTHOTicS1D8G8Nq06UCQAtJ4StsTHKm0kFKylPXGEN+PsADx9lLzSIN+WRupI8N
+              jmHH9trHsfLkjyZ9DxzUmnzyJnzovTX+3ImPG1aQHYw=`
+ };
